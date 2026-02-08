210f4eb947092454609abf8770f84b9b6a7ed985
shalinichouhan024 <shalinichouhan024@gmail.com>
2026-02-08 11:43:13 +0530

    Introduced db for Person

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloController {
+
+    private final PersonRepository personRepository;
+
+    public HelloController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping("/")
+    public String greet(){
+        return "Hello Shalini from Cloud Run via GitHub Actions!!";
+    }
+
+    @GetMapping("/api/data")
+    public List<Person> getData() {
+        return personRepository.findAll();
+    }
+}
